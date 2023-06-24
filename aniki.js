16a13768c97b1e382d83bb7138dfea83201e04bd
Normi offi <88531106+Normioffi@users.noreply.github.com>
2023-06-24 15:03:10 +0200

    Update aniki.js

--- aniki.js
+++ aniki.js
@@ -1,19 +1,27 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
-const { apiEPP } = require("./endpointParams/EPP.js");
+const { apiEPP } = require("./endPointParams/EPP.js");
 
 
-class AnikiApiType {
+class AnikiAnimeApi {
     constructor(api, isDev) {
         this.api = api;
         this.isDev = isDev;
     }
+
 };
-class Aniki extends AnikiApiType {
-/**
- * 
- * @param {string} api - The api used for the request.
- * @param {boolean} isDev - If true: returns console server errors. If false: no returns console server errors.
- */
+
+class AnikiMangaApi {
+    constructor(api, isDev) {
+        this.api = api;
+        this.isDev = isDev;
+    }
+}
+class Anime extends AnikiAnimeApi {
+    /**
+     * 
+     * @param {string} api - The api used for the request.
+     * @param {boolean} isDev - If true: returns console server errors. If false: no returns console server errors.
+     */
     constructor(api, isDev) {
         super()
         this.api = api
@@ -29,10 +37,10 @@ class Aniki extends AnikiApiType {
      * @param {string} query - The query for research.
      * @param {number} offset - The offset for a certain result. (Optional)
      */
-    
+
     search(query, offset) {
         if (query) {
-            return fetch(this.url + apiEPP(this.api, query, offset).search, {
+            return fetch(this.url + apiEPP("anime", this.api, query, offset).search, {
                 headers: {
                     'Content-Type': 'application/vnd.api+json',
                     'Accept': 'application/vnd.api+json'
@@ -104,13 +112,13 @@ class Aniki extends AnikiApiType {
                 'Accept': 'application/vnd.api+json'
             }
         }).then(r => {
-             if (r.ok) {
+            if (r.ok) {
                 return r.json();
             }
             if (this.isDev === true) {
                 if (r.status === 404) {
                     console.warn("No data or invalid EndPoint or parameter.")
-                 } else if (r.status === 406) {
+                } else if (r.status === 406) {
                     console.error("invalid data in Accept. " + r);
                 } else {
                     throw new Error(r)
@@ -127,14 +135,13 @@ class Aniki extends AnikiApiType {
 
     /**
      * @param {string} type - The theme of the list.
-     * @param {number} offset - The offset for a specific result. (Optional)
      */
 
-    theme(type, offset) {
+    theme(type) {
         if (this.api === "kitsu") return console.warn("No theme endpoint for this api")
-           
+
         if (type) {
-            return fetch(this.url + apiEPP(this.api, type, offset).theme, {
+            return fetch(this.url + apiEPP(this.api, type).theme, {
                 headers: {
                     'Content-Type': 'application/vnd.api+json',
                     'Accept': 'application/vnd.api+json'
@@ -165,13 +172,12 @@ class Aniki extends AnikiApiType {
     }
     /**
      * @param {string} type - The gender for the list.
-     * @param {number} offset - The offset for a specific result. (Optional)
      */
 
-    gender(type, offset) {
+    gender(type) {
         if (this.api === "kitsu") return console.warn("No gender endpoint for this api")
         if (type) {
-            return fetch(this.url + apiEPP(this.api, type, offset).gender, {
+            return fetch(this.url + apiEPP(this.api, type).gender, {
                 headers: {
                     'Content-Type': 'application/vnd.api+json',
                     'Accept': 'application/vnd.api+json'
@@ -202,4 +208,87 @@ class Aniki extends AnikiApiType {
     }
 };
 
-module.exports = { Aniki };
+class Manga extends AnikiMangaApi {
+    /**
+         * 
+         * @param {string} api - The api used for the request.
+         * @param {boolean} isDev - If true: returns console server errors. If false: no returns console server errors.
+         */
+    constructor(api, isDev) {
+        super();
+        this.api = api;
+        if (api === "kitsu") {
+            this.url = "https://kitsu.io/api/edge/";
+        } else {
+            this.url = "https://kitsu.io/api/edge";
+        }
+        this.isDev = isDev;
+    }
+    /**
+     * @param {string} query - The query for research.
+     * @param {number} offset - The offset for a certain result. (Optional)
+     */
+
+    search(query, offset) {
+        if (query) {
+            return fetch(this.url + apiEPP("manga", this.api, query, offset).search, {
+                headers: {
+                    'Content-Type': 'application/vnd.api+json',
+                    'Accept': 'application/vnd.api+json'
+                }
+            }).then(r => {
+                if (r.status === 200) {
+                    return r.json();
+                }
+                if (this.isDev === true) {
+                    if (r.status === 404) {
+                        console.warn("No data or invalid EndPoint or parameter.")
+                    } else if (r.status === 406) {
+                        console.error("invalid data in Accept. " + r);
+                    } else {
+                        throw new Error(r)
+                    }
+                } else if (this.isDev === false) {
+                    return null;
+                } else {
+                    console.error("The parameter isDev must be a boolean!")
+                }
+            });
+        } else {
+            console.error("No text entries.")
+        }
+    }
+
+    /**
+     * 
+     * @param {number} offset - The offset for an page.
+     */
+    list(offset) {
+        return fetch(this.url + apiEPP(this.api, ".", offset).list, {
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }).then(r => {
+            if (r.ok) {
+                return r.json();
+            }
+            if (this.isDev === true) {
+                if (r.status === 404) {
+                    console.warn("No data or invalid EndPoint or parameter.")
+                } else if (r.status === 406) {
+                    console.error("invalid data in Accept. " + r);
+                } else {
+                    throw new Error(r)
+                }
+            } else if (this.isDev === false) {
+                return null;
+            } else {
+                console.error("The parameter isDev must be a boolean!")
+            }
+        }).catch(e => {
+            console.error('An error was occured', e);
+        })
+    }
+}
+module.exports = { Anime, Manga };
