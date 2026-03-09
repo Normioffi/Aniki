683e1e96e3442e227f4fbd079d06ccdd621e2f45
Normi offi <88531106+Normioffi@users.noreply.github.com>
2026-03-09 16:02:40 +0100

    .

--- src/apis/kitsu/index.js
+++ src/apis/kitsu/index.js
@@ -1,5 +1,3 @@
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const { MangaKitsu } = require("./manga");
 const { AnimeKitsu } = require("./anime");
 
