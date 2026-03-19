6c5f1519704f1f85205028b4463a77a9c0bf7e00
Normi offi <88531106+Normioffi@users.noreply.github.com>
2026-03-19 14:49:38 +0100

    Improving exportations of all classes, function and constants.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-const { AnimeKitsu, MangaKitsu } = require("./apis/kitsu/index.js");
-const { MyAnimeList, MyMangaList } = require("./apis/myanimelist/index.js");
-const { WaifuIm } = require("./apis/waifuim/index.js");
-const { WaifuIt } = require("./apis/waifuit/index.js");
-const { isSameArray } = require("./funcs/index.js");
-
-const { kitsu, myanimelist } = require("./consts/index.js");
-
 module.exports = {
-  // Classes and functions
-  AnimeKitsu,
-  MangaKitsu,
-  isSameArray,
-  MyAnimeList,
-  MyMangaList,
-  WaifuIm,
-  WaifuIt,
-  // Constants
-  kitsu,
-  myanimelist,
+  ...require("./apis/myanimelist/index.js"),
+  ...require("./apis/kitsu/index.js"),
+  ...require("./apis/waifuim/index.js"),
+  ...require("./apis/waifuit/index.js"),
+  ...require("./funcs/index.js"),
+  ...require("./consts/index.js"),
 };
