33b1147cfa4223913addb2f93fb153e14240ace5
Normi offi <88531106+Normioffi@users.noreply.github.com>
2026-03-19 09:55:44 +0100

    Adding the defaultHandleError function, editing Fetchconfig and adding a @since, adding a desc, template and since for the AnikiHooks type and more.

--- types/core/index.d.ts
+++ types/core/index.d.ts
@@ -1,26 +1,57 @@
 import { Response } from "node-fetch";
-import { IKitsuError } from "../kitsu";
-import { IMALError } from "../myanimelist";
-import type { IWaifuImError } from "../waifuim";
+import type { IKitsuError } from "../apis/kitsu/interfaces";
+import type { IMALError } from "../apis/myanimelist/interfaces";
+import type { IWaifuImError } from "../apis/waifuim/interfaces";
+import type { IWaifuItError } from "../apis/waifuit/interfaces";
 
+/**
+ * List of every error interfaces.
+ * @since 2.0.0-beta.1
+ */
+type ErrorsHook = IWaifuImError | IMALError | IKitsuError | IWaifuItError;
+
+/**
+ * @since 2.0.0-beta.1
+ */
 interface FetchConfig {
   url: string;
   headers: {
     [name: string | number]: string | Object;
   };
-  parameters?: URLSearchParams;
   endpoint?: string;
+  parameters?: URLSearchParams;
 }
-type AnikiHooks<Err extends IWaifuImError | IMALError | IKitsuError> = {
+
+/**
+ * Type for hooks that can optionnally be executed before or after a request and on error.
+ * @template Err - Generic type for changing the possible error response.
+ * @since 2.0.0-beta.1
+ */
+type AnikiHooks<Err extends ErrorsHook | unknown> = {
   beforeRequest: (config: FetchConfig) => Promise<void>;
   afterRequest: (res: Response) => Promise<void>;
   onError: (error: Promise<Err>, res: Response) => Promise<void>;
 };
 
+/**
+ * @class
+ *
+ *
+ * @since 2.0.0-beta.1
+ */
 declare class AnikiCore {
+  private defaultHandleError: (error) => Promise<void>;
+
+  /**
+   * @method
+   *
+   * @description d
+   * @param config
+   * @param hooks
+   */
   fetching(
     config: FetchConfig,
-    hooks: AnikiHooks,
+    hooks: AnikiHooks<undefined>,
   ): Promise<Response | undefined>;
 }
 
