2fc30dd4265ea5270930bf6a71ebc9ca1819c9c4
Normi offi <88531106+Normioffi@users.noreply.github.com>
2026-03-20 14:18:24 +0100

    New @param hooks description.

--- types/apis/waifuim/index.d.ts
+++ types/apis/waifuim/index.d.ts
@@ -22,6 +22,7 @@ declare class WaifuIm extends AnikiCore {
    * @description Find an waifu with different parameters (only tags like: maid, waifu, marin-kitagawa, mori-calliope, raiden-shogun, oppai, selfies or uniform can be used.) For more informations, go to https://docs.waifu.im/ website.
    *
    * @param params - The optionals parameters for the request.
+   * @param hooks Object with functions inside to execute code before, after a request and on error.
    *
    * @throws An internal server error has occured: **error**
    * @returns Return a Promise.
