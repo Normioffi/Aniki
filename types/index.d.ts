b54e9871811c77827d8d01afe591410f2db8e2e8
Normi offi <88531106+Normioffi@users.noreply.github.com>
2026-04-21 15:46:23 +0200

    Danbooru export in d.ts is now a comment.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,3 @@
-export * from "./apis/danbooru/index";
 export * from "./apis/kitsu/index";
 export * from "./apis/myanimelist/index";
 export * from "./apis/waifuim/index";
@@ -6,3 +5,4 @@ export * from "./consts/index";
 export * from "./funcs/index";
 
 // export * from "./apis/waifuit/index";
+// export * from "./apis/danbooru/index";
