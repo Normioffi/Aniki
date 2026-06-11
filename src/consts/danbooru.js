1b3c3c8a6b9eb5cc6f33fc44983ce29713f2b482
Normi offi <88531106+Normioffi@users.noreply.github.com>
2026-06-11 16:21:32 +0200

    Adding Safebooru url as `SafeDanUrl`.

--- src/consts/danbooru.js
+++ src/consts/danbooru.js
@@ -4,4 +4,6 @@ const TestDanUrl = "https://testbooru.donmai.us";
 
 const DanRatings = Object.freeze(["g", "s", "q", "e"]);
 
-module.exports = { DanUrl, TestDanUrl, DanRatings };
+const SafeDanUrl = "https://safebooru.org";
+
+module.exports = { DanUrl, SafeDanUrl, TestDanUrl, DanRatings };
